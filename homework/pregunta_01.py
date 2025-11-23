92d554ffe36a096c048d6ddda3300392e6f3c6a2
jotorresbe <jotorresbe@unal.edu.co>
2025-11-23 18:31:11 -0500

    Taller 08 Realizado

---
+++ homework/pregunta_01.py
@@ -0,0 +1,142 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+def load_data():
+    data = pd.read_csv("files/input/shipping-data.csv")
+    return data
+def create_visual_for_shipping_per_warehouse(data):
+    df = data.copy()
+    plt.figure()
+    counts = df['Warehouse_block'].value_counts()
+    counts.plot.bar(
+        title='Shippings per Warehouse Block',
+        xlabel='Warehouse Block',
+        ylabel='Record count',
+        color='tab:blue',
+        fontsize=8,
+    )
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.savefig("docs/shipping_per_warehouse.png")
+def create_visual_for_mode_of_shipment(data):
+    df = data.copy()
+    plt.figure()
+    counts = df['Mode_of_Shipment'].value_counts()
+    counts.plot.pie(
+        title='Mode of Shipment',
+        wedgeprops=dict(width=0.35),
+        ylabel="",
+        colors=['tab:blue', 'tab:orange', 'tab:green'],
+    )
+    plt.savefig("docs/mode_of_shipment.png")
+
+def create_visual_for_average_customer_rating(data):
+    df = data.copy()
+    plt.figure()
+    df = (
+        df[["Mode_of_Shipment", "Customer_rating"]]
+        .groupby("Mode_of_Shipment")
+        .describe()
+    )
+    df.columns = df.columns.droplevel()
+    df = df[["mean","min", "max"]]
+
+    plt.barh(
+        y = df.index.values,
+        width=df["max"].values -1,
+        left= df['min'].values,
+        height=0.9,
+        color="lightgray",
+        alpha=0.8,
+    )
+
+    colors = [
+        "tab:green" if value >=3.0 else "tab:orange" for value in df["mean"].values
+    ]
+
+    plt.barh(
+        y = df.index.values,
+        width=df["mean"].values -1,
+        left= df['min'].values,
+        height=0.5,
+        color=colors,
+        alpha=1.0
+    )
+
+    plt.title("Average Customer")
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.savefig("docs/average_customer_rating.png")
+def create_visual_for_weight_distribution(data):
+    df = data.copy()
+    plt.figure()
+    df['Weight_in_gms'].plot.hist(
+        bins=30,
+        title='Shipped Weight Distribution',
+        color='tab:orange',
+        edgecolor='white',
+    )
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.savefig("docs/weight_distribution.png")
+def pregunta_01():
+    """
+    El archivo `files//shipping-data.csv` contiene información sobre los envios
+    de productos de una empresa. Cree un dashboard estático en HTML que
+    permita visualizar los siguientes campos:
+
+    * `Warehouse_block`
+
+    * `Mode_of_Shipment`
+
+    * `Customer_rating`
+
+    * `Weight_in_gms`
+
+    El dashboard generado debe ser similar a este:
+
+    https://github.com/jdvelasq/LAB_matplotlib_dashboard/blob/main/shipping-dashboard-example.png
+
+    Para ello, siga las instrucciones dadas en el siguiente video:
+
+    https://youtu.be/AgbWALiAGVo
+
+    Tenga en cuenta los siguientes cambios respecto al video:
+
+    * El archivo de datos se encuentra en la carpeta `data`.
+
+    * Todos los archivos debe ser creados en la carpeta `docs`.
+
+    * Su código debe crear la carpeta `docs` si no existe.
+
+    """
+    os.makedirs("docs", exist_ok=True)
+    df = load_data()
+    create_visual_for_shipping_per_warehouse(df)
+    create_visual_for_mode_of_shipment(df)
+    create_visual_for_average_customer_rating(df)
+    create_visual_for_weight_distribution(df)
+notebook_dir = os.getcwd()
+parent_dir = os.path.dirname(notebook_dir)
+docs_dir = os.path.join(parent_dir, "docs")
+
+output_path = os.path.join(docs_dir, "index.html")
+
+html_content = """
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1>Shipping Dashboard Example</h1>
+            <div style="width:45%; float:left">
+                <img src="shipping_per_warehouse.png" alt="Fig 1">
+                <img src="mode_of_shipment.png" alt="Fig 2">
+            </div>
+            <div style="width:45%; float:left">
+                <img src="average_customer_rating.png" alt="Fig 3">
+                <img src="weight_distribution.png" alt="Fig 4">
+            </div>
+        </body>
+    </html> """
+
+with open(output_path, "w") as f:
+    f.write(html_content)
